6bf8f3d94bea9a3e9bb4516e1abeda84d18cc258
unknown <gwang907@gmail.com>
2025-03-12 20:07:16 +0900

    simplized main()

--- bin/main.dart
+++ bin/main.dart
@@ -13,6 +13,8 @@ class ShoppingMall {
   int totalPrice = 0;
   List<String> totalName = [];
 
+  bool isOpen = true; //쇼핑몰 오픈 여부
+
   void printProducts() {
     for (Product p in productList) {
       print('${p.name} / ${p.price} 원');
@@ -80,25 +82,58 @@ class ShoppingMall {
     }
     totalPrice += productPrice[productName.indexOf(inputPName)] * inputDemand;
     totalName.add(inputPName);
+    print('$inputPName $inputDemand개가 장바구니에 담겼습니다!'); //장바구니에 상품 추가 안내 문구 출력력
   }
 
   void showTotal() {
-    print('장바구니에 ${totalName.join(',')}(이)가 담겨있네요. 총 $totalPrice원 입니다!');
+    //장
+    if (totalPrice > 0) {
+      print('장바구니에 ${totalName.join(',')}(이)가 담겨있네요. 총 $totalPrice원 입니다!');
+    } else {
+      print('장바구니가 비어있습니다.');
+    }
   }
 
   void clearCart() {
-    if (totalPrice > 0) {
-      totalPrice = 0;
-      print('장바구니를 초기화합니다.');
+    print('장바구니를 초기화하시겠습니까?');
+    print('[6] : 확인 / [그 외 입력] : 취소');
+    var check = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
+    if (check != null) {
+      if (check == '6') {
+        if (totalPrice > 0) {
+          totalName.clear();
+          totalPrice = 0;
+          print('장바구니를 초기화합니다.');
+        } else {
+          print('이미 장바구니가 비어있습니다.');
+        }
+      } else {
+        print('취소했습니다.');
+      }
     } else {
-      print('이미 장바구니가 비어있습니다.');
+      print('지원하지 않는 기능입니다 ! 다시 시도해 주세요 . .');
     }
+  }
+
+  void exit() {
+    print('정말 종료하시겠습니까?');
+    print('[5] : 종료 / [그 외 입력] : 계속');
+    var check = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
+    if (check != null) {
+      if (check == '5') {
+        print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
+        isOpen = false;
+      } else {
+        print('종료하지 않습니다.');
+      }
+    } else {}
   }
 }
 
 void main() {
   ShoppingMall mall = ShoppingMall();
 
+  /* 이하 for문으로 개선된 기존 코드
   Product shirts = Product('셔츠', 10000);
   Product pants = Product('바지', 20000);
   Product skirt = Product('치마', 30000);
@@ -110,18 +145,33 @@ void main() {
   mall.productList.add(pants);
   mall.productList.add(socks);
   mall.productList.add(shoes);
+  */
 
-  bool isOpen = true; //쇼핑몰 상태 관리
+  Map<String, int> products = {
+    '셔츠': 10000,
+    '바지': 20000,
+    '치마': 30000,
+    '양말': 1000,
+    '신발': 50000,
+  };
 
-  while (isOpen) {
+  List<String> keys = products.keys.toList();
+  List<int> values = products.values.toList();
+
+  for (var i = 0; i < products.length; i++) {
+    Product product = Product(keys[i], values[i]);
+    mall.productList.add(product);
+  }
+
+  while (mall.isOpen) {
     print(
-      '------------------------------------------------------------------------------------------------------------------------',
+      '----------------------------------------------------------------------------------------------------------------------------',
     );
     print(
       '[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료 / [6] 장바구니 비우기',
     );
     print(
-      '------------------------------------------------------------------------------------------------------------------------',
+      '----------------------------------------------------------------------------------------------------------------------------',
     );
     var input = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
 
@@ -132,35 +182,9 @@ void main() {
     } else if (input == '3') {
       mall.showTotal();
     } else if (input == '4') {
-      print('정말 종료하시겠습니까?');
-      print('[5] : 종료 / [그 외 입력] : 계속');
-      var check = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
-      if (check != null) {
-        if (check == '5') {
-          print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
-          isOpen = false;
-        } else {
-          print('종료하지 않습니다.');
-          continue;
-        }
-      } else {
-        continue;
-      }
+      mall.exit();
     } else if (input == '6') {
-      print('장바구니를 초기화하시겠습니까?');
-      print('[6] : 확인 / [그 외 입력] : 취소');
-      var check = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
-      if (check != null) {
-        if (check == '6') {
-          mall.clearCart();
-        } else {
-          print('취소했습니다.');
-          continue;
-        }
-      } else {
-        print('지원하지 않는 기능입니다 ! 다시 시도해 주세요 . .');
-        continue;
-      }
+      mall.clearCart();
     } else {
       print('지원하지 않는 기능입니다 ! 다시 시도해 주세요 . .');
     }
